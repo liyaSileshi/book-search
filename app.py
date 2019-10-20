ec5168b987b5492b9e3a10e4ee9c4e9084227dd7
liyaSileshi <liyasileshi@gmail.com>
2019-10-19 23:45:08 -0700

    search of books works

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request, url_for
+import requests
+import json
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET'])
+def booksearch():
+    # TODO: Extract query term from url
+    if request.method == 'GET':
+        search_term = request.args.get('q')
+    #search_term = request.args.get("q")
+
+    # TODO: Make an API call to Tenor using the 'requests' library
+    
+        response = requests.get(f'https://www.googleapis.com/books/v1/volumes?q={search_term}&maxResults=6')
+        
+        # TODO: Get the first 10 results from the search results
+        # books = json.loads(response.content)
+        books = response.json()
+        img = books['items'][0]['volumeInfo']['imageLinks']['thumbnail']
+    # return author
+    return render_template('index.html', img = img, search_term= search_term )
+
+    
+
+
+if __name__ == '__main__':
+    # gifsearch()
+    app.run(debug=True)
